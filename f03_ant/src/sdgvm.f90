67d5947ad8a6331b373a3c244a859ad7037c155d
epik78 <evripidis78@yahoo.gr>
2018-09-06 11:02:58 +0100

    c

--- f03_ant/src/sdgvm.f90
+++ f03_ant/src/sdgvm.f90
@@ -1,6 +1,6 @@
 ! ==================================================================== !
 ! Name        : sdgvm.f90
-! Author      : Mark Lomas
+! Author      : Mark Lomas good
 ! Version     : 0.1 Comments
 ! Copyright   : Your copyright notice
 ! Description : sdgvm
